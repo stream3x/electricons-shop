996cadbb7dea3556c73ddef1eb20bd7c78aab9b6
CikaDraza <drazic.milan@gmail.com>
2023-07-04 07:28:53 +0200

    redeploy blog comments performance

--- src/utils/db.js
+++ src/utils/db.js
@@ -15,10 +15,9 @@ async function connect() {
     }
     await mongoose.disconnect();
   }
-  const db = mongoose.connect(process.env.MONGODB_URI, {
+  const db = await mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
+    useUnifiedTopology: true
   });
   console.log('new connection');
   connection.isConnected = db.connections[0].readyState;
